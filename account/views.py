afe27dc240127d12b69e3dd23dcffab6be8efd86
Akira Shirase <51890190+brokfinger@users.noreply.github.com>
2020-09-05 18:48:32 +0300

    Added column for news, but little problem with 'split' in (filter) object.

--- account/views.py
+++ account/views.py
@@ -1,20 +1,31 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.http import require_POST
+from django.contrib.auth.models import User
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404
-from django.contrib import messages
+from django.views.decorators.http import require_POST
 
+from actions.models import Action
+from actions.utils import create_action
+from bookmarks.common.decorators import ajax_required
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, \
     ProfileEditForm
 from .models import Profile, Contact
-from bookmarks.common.decorators import ajax_required
 
 
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+    actions = Action.objects.exclude(user=request.user)
+    # following_ids = request.user.following.values_list(id, flat=True)
+    # if following_ids:
+    #     actions = actions.filter(user_id__in=following_ids)
+    #
+    # actions = actions.select_related('user', 'user__profile').prefetch_related(
+    #     'target')[:10]
+
+    return render(request, 'account/dashboard.html',
+                  {'section': 'dashboard', 'actions': actions})
 
 
 def user_login(request):
@@ -46,6 +57,7 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password2'])
             new_user.save()
             new_user = Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request, 'account/register_done.html',
                           {'new_user': new_user})
     else:
@@ -100,6 +112,7 @@ def user_follow(request):
             if action == 'follow':
                 Contact.objects.get_or_create(user_from=request.user,
                                               user_to=user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_form=request.user,
                                        user_to=user).delete()
