efd452e2fe9749cd93f6b8c95d946a9e8b0a3ce6
Akira Shirase <51890190+brokfinger@users.noreply.github.com>
2020-10-23 19:40:03 +0300

    ...

--- images/views.py
+++ images/views.py
@@ -1,3 +1,6 @@
+import redis
+
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -5,11 +8,14 @@ from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 
-from bookmarks.common.decorators import ajax_required
 from actions.utils import create_action
-
+from bookmarks.common.decorators import ajax_required
 from .forms import ImageCreateForm
 from .models import Image
+
+# for use in handlers
+r = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_HOST,
+                      db=settings.REDIS_DB)
 
 
 @login_required
@@ -33,8 +39,10 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    total_views = r.incr('image:{}:views'.format(image.id))
     return render(request, 'images/image/detail.html',
-                  {'section': 'images', 'image': image})
+                  {'section': 'images', 'image': image,
+                   'total_views': total_views})
 
 
 @ajax_required
